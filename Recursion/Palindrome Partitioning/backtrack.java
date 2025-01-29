1bef437f9f79df3e8ccecf210c1f84dc8ee45745
bitwise-ar <rajayush1412@gmail.com>
2025-01-29 23:05:55 +0530

    Palindrome Partitioning

--- Recursion/Palindrome Partitioning/backtrack.java
+++ Recursion/Palindrome Partitioning/backtrack.java
@@ -5,7 +5,7 @@ public class backtrack{
             List<String> part = new ArrayList<>();
             helper(0, s, part, res);
             return res;
-        }
+        } 
     
         public void helper(int idx, String s, List<String> part, List<List<String>> res){
             if(idx == s.length()){
@@ -15,7 +15,7 @@ public class backtrack{
     
             for(int i = idx; i < s.length(); i++){
                 if(isPalindrome(s, idx, i)){ 
-                    part.add(s.substring(idx, i + 1)); 
+                    part.add(s.substring(idx, i + 1));
                     helper(i + 1, s, part, res); 
                     part.removeLast(); 
                 }
