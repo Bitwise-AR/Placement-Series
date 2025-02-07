aaaae0e67d1696638be50bc09cce74c30e0cef42
bitwise-ar <rajayush1412@gmail.com>
2025-02-07 20:29:33 +0530

    Minimum path sum in Triangular Grid

---
+++ Dynamic Programming/Triangle/recursive_memoization.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class recursive_memoization {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+
+        for(int[] row : dp)
+        {
+            Arrays.fill(row , -1);
+        }
+
+        return f(0,0,triangle,dp,n);
+    }
+    private static int f(int i, int j, List<List<Integer>> num, int[][] dp, int n) {
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(i==n-1) return num.get(i).get(j);
+
+        int down = num.get(i).get(j) + f(i+1,j,num,dp,n);
+        int diagonal = num.get(i).get(j) + f(i+1,j+1,num,dp,n);
+
+        return dp[i][j] = Math.min(down,diagonal);    
+    }
+}
