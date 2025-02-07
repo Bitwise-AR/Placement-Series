fdce6e20f9203011316cb7e5ec4766cf41a5250d
bitwise-ar <rajayush1412@gmail.com>
2025-02-07 20:29:04 +0530

    Unique Paths

---
+++ Dynamic Programming/Unique Paths II/tabulation.java
@@ -0,0 +1,36 @@
+class tabulation {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        
+        // If Start or End = obstacle
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+        
+        int[] above = new int[n];
+        above[0] = 1;  
+        
+        for (int row = 0; row < m; row++) {
+            int[] current = new int[n];
+            for (int col = 0; col < n; col++) {
+                if (obstacleGrid[row][col] == 1) {
+                    current[col] = 0; // Obstacle 
+                } else {
+                    if (col == 0 && row == 0) {
+                        current[col] = 1; 
+                    } else {
+                        if (col > 0) {
+                            current[col] += current[col - 1];
+                        }
+                        if (row > 0) {
+                            current[col] += above[col];
+                        }
+                    }
+                }
+            }
+            above = current;
+        }
+        
+        return above[n - 1];
+    }
+}
