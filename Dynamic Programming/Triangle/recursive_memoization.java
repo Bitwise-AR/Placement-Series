4a10b5f822468bfe4a3fcecfce1c92009ce18769
bitwise-ar <rajayush1412@gmail.com>
2025-02-14 11:05:05 +0530

    Triangle

--- Dynamic Programming/Triangle/recursive_memoization.java
+++ Dynamic Programming/Triangle/recursive_memoization.java
@@ -10,15 +10,15 @@ public class recursive_memoization {
             Arrays.fill(row , -1);
         }
 
-        return f(0,0,triangle,dp,n);
+        return helper(0,0,triangle,dp,n);
     }
-    private static int f(int i, int j, List<List<Integer>> num, int[][] dp, int n) {
+    private static int helper(int i, int j, List<List<Integer>> num, int[][] dp, int n) {
         if(dp[i][j] != -1) return dp[i][j];
 
         if(i==n-1) return num.get(i).get(j);
 
-        int down = num.get(i).get(j) + f(i+1,j,num,dp,n);
-        int diagonal = num.get(i).get(j) + f(i+1,j+1,num,dp,n);
+        int down = num.get(i).get(j) + helper(i+1,j,num,dp,n);
+        int diagonal = num.get(i).get(j) + helper(i+1,j+1,num,dp,n);
 
         return dp[i][j] = Math.min(down,diagonal);    
     }
