980f8168e0b1a4ceb436109c9bdd26856105468b
bitwise-ar <rajayush1412@gmail.com>
2025-02-17 22:44:51 +0530

    Min Deletion to make two strings same

---
+++ Dynamic Programming/Min Deletion to make two strings same/memoization.java
@@ -0,0 +1,24 @@
+class memoization {
+    private int helper(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n]; 
+    }
+
+    public int minDistance(String word1, String word2) {
+        int lcs = helper(word1, word2);
+        return (word1.length() - lcs) + (word2.length() - lcs);
+    }    
+}
