de792952c5dfa6f1756bc084ca09af691e257326
Ankux <ankuxkr@gmail.com>
2025-02-23 11:08:00 +0530

    Matrix Chain Multiplication

---
+++ Dynamic Programming/DP on MCM - Partition DP/Matrix Chain Multiplication/tabulation.cpp
@@ -0,0 +1,19 @@
+int matrixMultiplication(vector<int> &arr) {
+    int n = arr.size();
+    int dp[n][n];
+    
+    for(int i=1; i<n; i++) dp[i][i] = 0;
+    
+    for(int i=n-1; i>=1; i--){
+        for(int j=i+1; j<n; j++){
+            int mini = 1e9;
+            for(int k=i; k<=j-1; k++){
+                int temp = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+                mini = min(mini, temp);
+            }
+            dp[i][j] = mini;
+        }
+    }
+    
+    return dp[1][n-1];
+}
