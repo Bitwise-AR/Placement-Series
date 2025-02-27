ebc12959da675f964815aee1665643157a64fa28
bitwise-ar <rajayush1412@gmail.com>
2025-02-27 20:34:07 +0530

    Postorder Traversal

--- Binary Trees/Traversals/Postorder Traversal/iterative(2 stack).java
+++ Binary Trees/Traversals/Postorder Traversal/iterative(2 stack).java
@@ -1,23 +1,23 @@
 import java.util.*;
 
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+// public class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
 
-    TreeNode() {
-    }
+//     TreeNode() {
+//     }
 
-    TreeNode(int val) {
-        this.val = val;
-    }
+//     TreeNode(int val) {
+//         this.val = val;
+//     }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+//     TreeNode(int val, TreeNode left, TreeNode right) {
+//         this.val = val;
+//         this.left = left;
+//         this.right = right;
+//     }
+// }
 
 public class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
