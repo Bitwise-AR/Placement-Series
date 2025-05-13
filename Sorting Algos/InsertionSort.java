d8f17706bfc4d6277ad21e5b429fc95c621e80d2
bitwise-ar <rajayush1412@gmail.com>
2025-05-13 21:29:39 +0530

    Sorting

--- Sorting Algos/InsertionSort.java
+++ Sorting Algos/InsertionSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int arr[] = { 12, 11, 13, 5, 6 };
+        int[] arr = { 12, 11, 13, 5, 6 };
 
         int n = arr.length;
         for (int i = 1; i < n; ++i) {
